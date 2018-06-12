277e4cfee398054ba072bd30d8b3288427f96c6b
Ricardo Delgado <ricdelgado80@gmail.com>
2018-06-11 21:09:13 -0400

    handled auth

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
+
+// passports config
+require('./config/passport')(passport)
+
+// load routes
+const auth = require('./routes/auth')
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('It works')
 })
+
+// use routes
+app.use('/auth', auth)
 
 const port = process.env.PORT || 5000
 
